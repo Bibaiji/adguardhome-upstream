15d3db5ac07c3bbff5069334a9d800a56cfa1ce3
Bibaiji <130743705+Bibaiji@users.noreply.github.com>
2023-04-17 15:39:40 +0800

    Update upstream.sh

--- upstream.sh
+++ upstream.sh
@@ -2,7 +2,7 @@
 set -e
 DATE=`date --rfc-3339 sec`
 echo "$DATE: IPv4 connection testing..."
-if ping -c 3 "101.6.6.6" > /dev/null 2>&1; then
+if ping -c 3 "223.5.5.5" > /dev/null 2>&1; then
 	IPv4="true"
 fi
 echo "$DATE: IPv6 connection testing..."
@@ -12,25 +12,25 @@ fi
 if [[ $IPv4 == "true" ]]; then
 	if [[ $IPv6 == "true" ]]; then
 		echo "$DATE: IPv4 and IPv6 connections both available."
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6.conf > /dev/null 2>&1
+		curl -o "/var/tmp/default.upstream" https://raw.githubusercontent.com/Bibaiji/adguardhome-upstream/master/v6.conf > /dev/null 2>&1
 	else
 		echo "$DATE: IPv4 connection available."
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v4.conf > /dev/null 2>&1
+		curl -o "/var/tmp/default.upstream" https://raw.githubusercontent.com/Bibaiji/adguardhome-upstream/master/v4.conf > /dev/null 2>&1
 	fi
 else
 	if [[ $IPv6 == "true" ]]; then
 		echo "$DATE: IPv6 connection available."
-		curl -o "/var/tmp/default.upstream" https://gitlab.com/fernvenue/adguardhome-upstream/-/raw/master/v6only.conf > /dev/null 2>&1
+		curl -o "/var/tmp/default.upstream" https://raw.githubusercontent.com/Bibaiji/adguardhome-upstream/master/v6only.conf > /dev/null 2>&1
 	else
 		echo "ERROR: No available network connection was detected, please try again."
 		exit 1
 	fi
 fi
 echo "$DATE: Getting data updates..."
-curl -s https://gitlab.com/fernvenue/chn-domains-list/-/raw/master/CHN.ALL.agh | sed "/#/d" > "/var/tmp/chinalist.upstream"
+curl -s https://github.com/Potterli20/file/releases/download/dns-hosts/dns-adguardhome-whitelist_full.txt | sed "/#/d" > "/var/tmp/chinalist.upstream"
 echo "$DATE: Processing data format..."
 cat "/var/tmp/default.upstream" "/var/tmp/chinalist.upstream" > /usr/share/adguardhome.upstream
-if ! [[ $IPv4 == "true" ]]; then sed -i "s|114.114.114.114|2001:da8::666|g" /usr/share/adguardhome.upstream; fi
+if ! [[ $IPv4 == "true" ]]; then sed -i "s|8.8.8.8|2001:4860:4860::8888|g" /usr/share/adguardhome.upstream; fi
 echo "$DATE: Cleaning..."
 rm /var/tmp/*.upstream
 systemctl restart AdGuardHome
